4ff4336d70450b5d79735393dfc5ff5e56f0aed9
Assigeh Delanyo <delanyoassigeh@gmail.com>
2017-07-13 14:08:30 +0000

    comments

--- person.py
+++ person.py
@@ -1,10 +1,13 @@
+#defines age and print out 'What your age?'
 def get_age():
 	age = raw_input("Whats your age?")
-	return age
+	return age #returns age
 
+#defines name and print out 'whats your name?'
 def get_name():
 	return raw_input("whats your name?")
 
-age = get_age()
-name = get_name()
+age = get_age()#calling age
+name = get_name()#calling age
+
 print "Hey, " + name + ", you are " + age + "years old, dude!"
