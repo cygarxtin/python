d7916e7305977d85c88cfbce4e1738e000a9b6af
Assigeh Delanyo <delanyoassigeh@gmail.com>
2017-07-13 14:29:39 +0000

    comment

--- conditional.py
+++ conditional.py
@@ -1,15 +1,17 @@
-#i = 8
-#if(i % 2):
-#    print "Odd Number"
-#else:
-#    print "Even Number"
+#initialised i
+i = 8
+if(i % 2):
+    print "Odd Number"#print out 'Odd Number'
+else:
+    print "Even Number"#print out 'Even Number'
 
 
 
-num = [1,2,3,4,5,6,7,8,9,10]
+num = [1,2,3,4,5,6,7,8,9,10]#calling num
 ans = 0  
+#for loop statement
 for i in num:
 	if not(i % 2):
 		ans += i
- 
+ #print out the result = 30
 print ans
--- for_loops.py
+++ for_loops.py
@@ -1,21 +1,32 @@
-#for i in range(1,11):
-#	print i
-	
-#for j in [1, 2, 3]:
-#	print j
+#for loop statement with range
+for i in range(1,11):
+#print out [1,2,3,4,5,6,7,8,9,10]
+	print i
+
+#for loop statement without range	
+for j in [1, 2, 3]:
+#print out [1,2,3]
+	print j
+
+#for loop statement with three arguments
+for a in range(0, 20, 2):
+#print out [0,2,4,6,8,10,12,14,16,18]
+		print a
+
+#for loop statement printing out in asterisks
+for i in range(1, 5):
+#print out '****'
+          #'****'
+	  #'****'
+	  #'****'
+print "****"
 
 
-#for a in range(0, 20, 2):
-#		print a
+for i in range(1, 5):
+	print "*" * i
 
-#for i in range(1, 5):
-#	print "****"
-
-#for i in range(1, 5):
-#	print "*" * i
-
-#for i in range(3, 0, -1):
-#	print "*" * i
+for i in range(3, 0, -1):
+	print "*" * i
 
 #print ("Pattern C")
 #for e in range (11,0,-1):
